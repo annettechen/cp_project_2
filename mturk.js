937b356c4de522903a90b9059a79f75681070117
Annette Chen <AnnetteChen@Annettes-MBP.wv.cc.cmu.edu>
2016-02-09 23:33:50 -0500

    mturk updated

--- mturk.js
+++ mturk.js
@@ -24,7 +24,7 @@ function gup(name) {
 }
 
 //  Turkify the captioning page.
-$(document).ready(function () {
+$(document).ready(function() {
   // is assigntmentId is a URL parameter
   if((aid = gup("assignmentId"))!="" && $(form_selector).length>0) {
 
